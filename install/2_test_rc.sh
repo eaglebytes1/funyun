0e54bbd617545d9f6cf8f1efa5498c6b94c05d00
Joel Berendzen <joel@generisbio.com>
2017-11-27 10:49:57 -0700

    rc on Amazon

--- install/2_test_rc.sh
+++ install/2_test_rc.sh
@@ -8,12 +8,14 @@ error_exit() {
   >&2 echo "   \"sudo service funyun stop\"."
 }
 trap error_exit EXIT
-sudo cp ${FNY_ROOT}/etc/rc.d/funyun /etc/rc.d/funyun
-sudo chmod 555 /etc/rc.d/funyun
-sudo cp ${FNY_ROOT}/etc/conf.d/funyun /etc/conf.d/funyun
-sudo chmod 555 /etc/rc.d/funyun
+sudo cp ${FNY_ROOT}/etc/rc.d/funyun /etc/rc.d/init.d/funyun
+sudo chmod 555 /etc/rc.d/init.d/funyun
+sudo chown root:root /etc/rc.d/init.d/funyun
+sudo cp ${FNY_ROOT}/etc/conf.d/funyun /etc/sysconfig/funyun
+sudo chmod 555 /etc/sysconfig/funyun
+sudo chown root:root /etc/sysconfig/funyun
+echo "enabling service.."
+sudo chkconfig --add funyun
 echo "starting service..."
 sudo service funyun start
-echo "Stopping the service..."
-sudo service funyun stop
 trap - EXIT
